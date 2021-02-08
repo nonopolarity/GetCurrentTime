eeb7f70c6f032d75f37152047466dd96a0790cc4
Kin <winterheat@gmail.com>
2021-02-07 18:46:42 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
+// Demo: 
+
 function App() {
+  const [data, setData] = useState({});
+ const [timeSliceID, setTimeSliceID] = useState(0);
+  let foo = 0;
+
+  useEffect(() => {
+    fetch("http://worldtimeapi.org/api/timezone/America/Los_Angeles")
+      .then(res => res.json())
+      .then(dataFetched => {
+        setData(dataFetched);
+      });
+  }, [foo]);
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log("INTERVAL", timeSliceID, foo);
+      // timeSliceID++;
+      foo++;
+      setTimeSliceID(timeSliceID + 1);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<pre className="data">
+        {JSON.stringify(data, null, 4)}
+      </pre> 
     </div>
   );
 }
